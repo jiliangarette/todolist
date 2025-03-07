b130c96971874bcfa667e1398c90802b917fbf8c
Jilian Garette Abadia Abangan <jiliangarette@gmail.com>
2025-03-07 12:19:28 -0800

    feat: update backend configuration and models

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -8,6 +8,7 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/tasks/', include('tasks.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'), 
+                    # tbh i dont understand this code but ik this is jwt token :(
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Task(models.Model):
+    # a task that is optional for a user
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
@@ -9,3 +10,9 @@ class Task(models.Model):
 
     def __str__(self):
         return self.title 
+
+# MY NOTES
+# models.ForeignKey(user ... ) so a user can have many task
+# on_delete=models.CASCADE - if user is deleted, all tasks is deleted
+# nullable
+
