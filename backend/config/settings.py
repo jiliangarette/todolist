9e99409e4449444621689216585bf4aaad01a890
Jilian Garette Abadia Abangan <jiliangarette@gmail.com>
2025-03-07 06:00:57 -0800

    configure django with aws rds postgres

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -1,5 +1,7 @@
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -53,10 +55,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 
 
+load_dotenv()
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST", "localhost"),
+        "PORT": os.getenv("DB_PORT", "5432"),
     }
 }
 
